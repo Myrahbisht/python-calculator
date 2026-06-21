616bf93ec10767edcc5879b95af5c5e6368653c1
Myrah Bisht <myrahbisht@gmail.com>
2026-06-20 22:13:04 -0700

    Add calculator functions with divide by zero handling

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: zero no divido"
+    else:
+        return a / b
+
+print(add(10, 5))
+print(subtract(10, 5))
+print(multiply(10, 5))
+print(divide(10, 5))
+print(divide(10, 0))
+
